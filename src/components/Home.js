801c8bd1b5440f8be881c1563bf8eee25c4b34b5
inbantamizhselvan <inbantamizhselvan@gmail.com>
2024-07-19 14:51:49 -0400

    Responsive home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import profile from '../assets/images/profile.jpg';
 import resume from '../assets/resume.pdf';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './Home.css';
 
 function Home() {
   const [text, setText] = useState('');
   const [index, setIndex] = useState(0);
-
 
   const fullText =
     "I am a Computer Science professional currently pursuing a Master of Science in Computer and Information Science at the University of Massachusetts Dartmouth.";
@@ -25,15 +25,21 @@ function Home() {
   return (
     <section id="home" className="home">
       <div className="container">
-        <div className="profile-pic-wrapper">
-          <img src={profile} alt="Profile" className="profile-pic" />
-        </div>
-        <div className="text-wrapper">
-          <h1 className="name">Inban Tamizhselvan</h1>
-          <div className="typing">
-            <p>{text}</p>
+        <div className="row justify-content-center align-items-center">
+          <div className="col-12 col-sm-6 col-md-4 text-center mb-4 mb-md-0">
+            <div className="profile-pic-wrapper">
+              <img src={profile} alt="Profile" className="profile-pic img-fluid" />
+            </div>
           </div>
-          <a href={resume} target="_blank" rel="noopener noreferrer" className="download-resume-btn">Download Resume</a>
+          <div className="col-12 col-sm-6 col-md-8 text-center text-sm-left text-md-left">
+            <div className="text-wrapper">
+              <h1 className="name">Inban Tamizhselvan</h1>
+              <div className="typing">
+                <p>{text}</p>
+              </div>
+              <a href={resume} target="_blank" rel="noopener noreferrer" className="download-resume-btn">Download Resume</a>
+            </div>
+          </div>
         </div>
       </div>
     </section>
